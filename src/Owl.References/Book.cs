13966e4d16a7cd013d9dee83453e2b3d5e735917
Johanna Petersson <min.mail@johannap.se>
2017-02-12 14:58:16 +0100

    Start of libary in .net 4.5.2

---
+++ src/Owl.References/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Owl.References
+{
+    public class Book
+    {
+        public Book()
+        {
+
+        }
+
+        public static Book CreateFull(string title, IEnumerable<Author> authors, int edition, string placeOfPublication,
+            string publisher, int yearOfPublication, string isbn, int startPage = -1, int endPage = -1)
+        {
+            return null;
+        }
+
+        public Guid Id { get; set; }
+
+        public string Title { get; private set; }
+
+        public IEnumerable<Author> Author { get; set; }
+
+        public int Edition { get; private set; }
+
+        public string PlaceOfPublication { get; private set; }
+
+        public string Publisher { get; private set; }
+
+        public int YearOfPublication { get; private set; }
+
+        public int StartPage { get; private set; }
+
+        public int EndPage { get; private set; }
+
+        public string ISBN { get; private set; }
+    }
+}
