4548ceaba1cc4d9abff98e07cd51993167d8933f
Johanna Petersson <min.mail@johannap.se>
2017-02-21 20:13:44 +0100

    Added project for parsing and started chicago for books

---
+++ src/Owl.References.Parsers/ChicagoManualStyleParser.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Owl.References.Parsers
+{
+    public class ChicagoManualStyleParser : ReferenceParser
+    {
+        public ChicagoManualStyleParser() { }
+
+        public Book Book(string reference)
+        {
+            var author = getAuthors(ref reference);
+            var title = getTitle(ref reference);
+            var edition = getEdition(ref reference);
+            var placeOfPubliction = getPlaceOfPublication(ref reference);
+            var publisher = getPublisher(ref reference);
+            var publicationYear = getYearOfPublication(ref reference);
+
+            return References.Book.CreateFull(title, new List<Author> {author}, edition, placeOfPubliction, publisher, publicationYear);
+        }
+
+        private static string getTitle(ref string reference)
+        {
+            var title = reference.Substring(0, reference.IndexOf(".", StringComparison.Ordinal));
+            reference = reference.Remove(0, title.Length + 2);
+            return title;
+        }
+
+        private static int getEdition(ref string reference)
+        {
+            var edition = 1;
+            if (reference.IndexOf(".", StringComparison.Ordinal) >= reference.IndexOf(":", StringComparison.Ordinal))
+                return edition;
+
+            var editionString = reference.Substring(0, reference.IndexOf(".", StringComparison.Ordinal));
+            reference = reference.Remove(0, editionString.Length + 2);
+            edition = Convert.ToInt32(Regex.Match(editionString, @"(^[0-9]*)").Value);
+
+            return edition;
+        }
+
+        private static string getPlaceOfPublication(ref string reference)
+        {
+            var placeOfPubliction = reference.Substring(0, reference.IndexOf(":", StringComparison.Ordinal));
+            reference = reference.Remove(0, placeOfPubliction.Length + 2);
+            return placeOfPubliction;
+        }
+
+        private static string getPublisher(ref string reference)
+        {
+            var publisher = reference.Substring(0, reference.IndexOf(",", StringComparison.Ordinal));
+            reference = reference.Remove(0, publisher.Length + 2);
+            return publisher;
+        }
+
+        private static int getYearOfPublication(ref string reference)
+        {
+            var publicationYear = reference.Substring(0, reference.IndexOf(".", StringComparison.Ordinal));
+            reference = reference.Remove(0, publicationYear.Length + 1);
+            return Convert.ToInt32(publicationYear);
+        }
+
+        private static Author getAuthors(ref string reference)
+        {
+            var authorString = reference.Substring(0, reference.IndexOf(".", StringComparison.Ordinal));
+            var authorParts = authorString.Split(new[] {", "}, StringSplitOptions.RemoveEmptyEntries);
+            reference = reference.Remove(0, authorString.Length + 2);
+            return new Author(authorParts[0], authorParts[1]);
+        }
+    }
+}
---
+++ src/Owl.Tests.UnitTests/References.Parsers/ChicagoManualStyleParser_Specs.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using Owl.References;
+using Owl.References.Parsers;
+
+namespace Owl.Tests.UnitTests.References.Parsers
+{
+    [TestFixture]
+    public class ChicagoManualStyleParser_Specs
+    {
+        private ChicagoManualStyleParser SUT;
+
+        [SetUp]
+        public void Setup()
+        {
+            SUT = new ChicagoManualStyleParser();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            SUT = null;
+        }
+
+        [Test]
+        public void should_be_ReferenceParser()
+        {
+            Assert.IsInstanceOf<ReferenceParser>(SUT);
+        }
+
+        #region Book
+
+        [Test]
+        public void book_should_handle_one_author()
+        {
+            // Given
+            var reference = "Faulkner, William. Absalom, Absalom!. New York: Vintage Books, 1990.";
+
+            // When
+            var result = SUT.Book(reference);
+
+            // Then
+            Assert.AreEqual(new Author("Faulkner", "William"), result.Authors.ElementAt(0));
+            Assert.AreEqual("Absalom, Absalom!", result.Title);
+            Assert.AreEqual("New York", result.PlaceOfPublication);
+            Assert.AreEqual("Vintage Books", result.Publisher);
+            Assert.AreEqual(1990, result.YearOfPublication);
+            Assert.AreEqual(1, result.Edition);
+        }
+
+        [Test]
+        public void book_should_handle_edition_of_book()
+        {
+            // Given
+            var reference = "Faulkner, William. Absalom, Absalom!. 2nd ed. New York: Vintage Books, 1990.";
+
+            // When
+            var result = SUT.Book(reference);
+
+            // Then
+            Assert.AreEqual(new Author("Faulkner", "William"), result.Authors.ElementAt(0));
+            Assert.AreEqual("Absalom, Absalom!", result.Title);
+            Assert.AreEqual("New York", result.PlaceOfPublication);
+            Assert.AreEqual("Vintage Books", result.Publisher);
+            Assert.AreEqual(1990, result.YearOfPublication);
+            Assert.AreEqual(2, result.Edition);
+        }
+
+        #endregion Book
+    }
+}
